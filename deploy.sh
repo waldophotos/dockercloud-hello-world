c2fccd1816f36fb13e2b49c41a11ab0c53797390
Adam Dyer <adamdyer@gmail.com>
2017-04-26 03:09:08 -0400

    more double rainbows

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ APP_ID=$3
 
 sed 's/<TAG>/$TAG/' marathon.json.template > marathon.json
 
-echo curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT https://ops.dcostest.waldo.photos/service/marathon/v2/apps/$APP_ID?force=true
+echo curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT "https://ops.dcostest.waldo.photos/service/marathon/v2/apps/${APP_ID}?force=true"
 
-curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT https://ops.dcostest.waldo.photos/service/marathon/v2/apps/$APP_ID?force=true
+curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT "https://ops.dcostest.waldo.photos/service/marathon/v2/apps/${APP_ID}?force=true"
 
