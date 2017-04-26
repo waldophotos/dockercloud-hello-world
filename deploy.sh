ba3f753af89253fa16db2b297305bff0040b1571
Adam Dyer <adamdyer@gmail.com>
2017-04-26 03:48:56 -0400

    what's in a name?

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,13 @@ TAG=$1
 
 JWT=$2
 
-sed "s/<TAG>/${TAG}/" marathon.json.template > marathon.json
+APP_NAME=$3
+
+sed -e "s/<TAG>/${TAG}/" -e "s/<APP_NAME>/${APP_NAME}/" marathon.json.template > marathon.json
 
 cat marathon.json
 
 #echo curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT "https://ops.dcostest.waldo.photos/service/marathon/v2/apps/hello-world?force=true"
 
-curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT "https://ops.dcostest.waldo.photos/service/marathon/v2/apps/hello-world?force=true"
+curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT "https://ops.dcostest.waldo.photos/service/marathon/v2/apps/${APP_NAME}?force=true"
 
