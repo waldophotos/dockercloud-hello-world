499a1c03f6a3056ae5be967d7b78063d8bc40133
Adam Dyer <adamdyer@gmail.com>
2017-04-26 03:03:15 -0400

    newlines?

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,9 @@ JWT=$2
 
 APP_ID=$3
 
-curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d "$(sed 's/<TAG>/$TAG/' marathon.json)" -X PUT https://ops.dcostest.waldo.photos/service/marathon/v2/apps/$APP_ID?force=true
+sed 's/<TAG>/$TAG/' marathon.json.template > marathon.json
+
+echo curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT https://ops.dcostest.waldo.photos/service/marathon/v2/apps/$APP_ID?force=true
+
+curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT https://ops.dcostest.waldo.photos/service/marathon/v2/apps/$APP_ID?force=true
 
