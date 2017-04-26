65c51616ef4e2a5427a8d32dffcd9ce3ca3fb5b5
Adam Dyer <adamdyer@gmail.com>
2017-04-26 02:05:20 -0400

    post not put

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ JWT=$2
 
 sed -e "s/<TAG>/$TAG/" marathon.json
 
-curl -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -X PUT -d @marathon.json https://ops.dcostest.waldo.photos/service/marathon/v2/apps
+curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json https://ops.dcostest.waldo.photos/service/marathon/v2/apps
 
 
 
