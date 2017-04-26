7b98fc798c1d1d2850ec9d2766912d5d5e31e104
Adam Dyer <adamdyer@gmail.com>
2017-04-26 03:25:51 -0400

    more debug

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ APP_ID=$3
 
 sed "s/<TAG>/${TAG}/" marathon.json.template > marathon.json
 
+cat marathon.json
+
 echo curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT "https://ops.dcostest.waldo.photos/service/marathon/v2/apps/${APP_ID}?force=true"
 
 curl -i -H "Authorization: token=${JWT}" -H 'Content-Type: application/json' -d @marathon.json -X PUT "https://ops.dcostest.waldo.photos/service/marathon/v2/apps/${APP_ID}?force=true"
